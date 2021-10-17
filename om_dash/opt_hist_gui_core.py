793042bcec63499545ee5c057389905fd20eec72
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2021-10-16 21:10:39 -0400

    Minor cleanup refactoring

--- om_dash/opt_hist_gui_core.py
+++ om_dash/opt_hist_gui_core.py
@@ -17,7 +17,6 @@ class GuiOptHistoryCore(PlotlyBase):
         self.include_dvs = True
 
         self.parser = RecorderParser(self.recorder_file)
-        self.parser.read_histories_from_recorder(self.recorder_file)
 
         sections = []
         sections.append(self.create_optimization_information_div())
@@ -30,6 +29,7 @@ class GuiOptHistoryCore(PlotlyBase):
                                    style=dict(backgroundColor='dodgerblue',
                                               fontSize='24px',
                                               color='white'))
+
         children = [html.H1('Case Information'),
                     self.create_case_information_input_table(),
                     start_button,
@@ -74,20 +74,16 @@ class GuiOptHistoryCore(PlotlyBase):
         return html.Div(children=children)
 
     def generate_opt_history_fig(self):
+        all_data = self._get_opt_history_data_from_parser()
 
-        if self.include_dvs:
-            all_data = self.parser.get_dataframe_of_all_data()
-        else:
-            all_data = self.parser.get_dataframe_of_objectives_and_constraints()
-
-        self.iterations = np.arange(all_data.shape[0])
+        self.plotted_iterations = np.arange(all_data.shape[0])
         on_secondary_y = self.determine_which_traces_to_put_on_2nd_y_axis(all_data)
-        include_y2 = any(on_secondary_y)
+        need_y2_axis = any(on_secondary_y)
 
         xaxis, yaxis = self.get_axis_settings()
         xaxis['title'] = 'Iteration'
         yaxis['title'] = 'Objective'
-        if include_y2:
+        if need_y2_axis:
             yaxis2 = self.get_secondary_y_axis_settings()
             yaxis2['title'] = 'Constraints and DVs' if self.include_dvs else 'Constraints'
         else:
@@ -97,7 +93,7 @@ class GuiOptHistoryCore(PlotlyBase):
         self.set_default_figure_layout(self.opt_hist_fig, xaxis, yaxis, yaxis2)
 
         for sec_y, (key, vals) in zip(on_secondary_y, all_data.items()):
-            self.opt_hist_fig.add_trace(go.Scattergl(x=self.iterations,
+            self.opt_hist_fig.add_trace(go.Scattergl(x=self.plotted_iterations,
                                                      y=vals,
                                                      mode='lines+markers',
                                                      name=key),
@@ -108,37 +104,57 @@ class GuiOptHistoryCore(PlotlyBase):
     def determine_which_traces_to_put_on_2nd_y_axis(self, all_data: pd.DataFrame):
         secondary_y = []
         for key in all_data.keys():
-            if key in self.parser.cons.keys():
+            if self._key_is_a_constraint_key(key):
                 secondary_y.append(True)
-            elif key in self.parser.dvs.keys():
+            elif self._key_is_a_dv_key(key):
                 secondary_y.append(True)
             else:
                 secondary_y.append(False)
         return secondary_y
 
+    def _key_is_a_constraint_key(self, key):
+        return key in self.parser.cons.keys()
+
+    def _key_is_a_dv_key(self, key):
+        return key in self.parser.dvs.keys()
+
     def generate_extend_data_for_opt_hist_traces(self):
-        if self.iterations.size == 0:
+        if not self._data_has_already_been_plotted():
             return dict(x=[], y=[])
 
-        if self.include_dvs:
-            all_data = self.parser.get_dataframe_of_all_data()
-        else:
-            all_data = self.parser.get_dataframe_of_objectives_and_constraints()
-        n_traces = all_data.shape[1]
-        extend_data = dict(x=[[] for _ in range(n_traces)],
-                           y=[[] for _ in range(n_traces)])
+        all_data = self._get_opt_history_data_from_parser()
 
-        if all_data.shape[0] > 0 and self.iterations.size > 0:
-            start = self.iterations[-1] + 1
+        n_traces = all_data.shape[1]
+        new_data = dict(x=[[] for _ in range(n_traces)],
+                        y=[[] for _ in range(n_traces)])
+        if self._valid_data_was_read(all_data):
+            start = self.plotted_iterations[-1] + 1
             new_iterations = np.arange(start, all_data.shape[0])
-            if new_iterations.size > 0:
-                extend_data = dict(x=[new_iterations.copy() for _ in range(n_traces)], y=[
-                                   val[new_iterations].to_numpy() for _, val in all_data.items()])
-                self.iterations = np.arange(new_iterations[-1]+1)
-        return extend_data
+
+            if self._have_new_data_to_plot(new_iterations):
+                new_data = dict(x=[new_iterations.copy() for _ in range(n_traces)],
+                                y=[val[new_iterations].to_numpy() for _, val in all_data.items()])
+                self.plotted_iterations = np.arange(new_iterations[-1]+1)
+        return new_data
+
+    def _get_opt_history_data_from_parser(self):
+        if self.include_dvs:
+            return self.parser.get_dataframe_of_all_data()
+        else:
+            return self.parser.get_dataframe_of_objectives_and_constraints()
+
+    def _valid_data_was_read(self, all_data):
+        return all_data.shape[0] > 0
+
+    def _data_has_already_been_plotted(self):
+        return self.plotted_iterations.size > 0
+
+    def _have_new_data_to_plot(self, new_iterations):
+        return new_iterations.size > 0
 
 
 def add_callbacks(app, core: GuiOptHistoryCore):
+
     @app.callback(
         [Output('live_update_interval', 'interval'),
          Output('opt_hist_graph', 'figure')],
@@ -146,17 +162,17 @@ def add_callbacks(app, core: GuiOptHistoryCore):
         [State('refresh_interval_input', 'value'),
          State('recorder_file', 'value'),
          State('include_dvs_checklist', 'value')])
-    def set_live_update_interval_and_initial_plots_div(
-            n_clicks, interval_sec, recorder_file, check):
-        core.include_dvs = True if 'DVS' in check else False
+    def set_live_update_interval_and_initial_plots_div(n_clicks, interval_in_seconds,
+                                                       recorder_file, dv_checklist):
+        core.include_dvs = True if 'DVS' in dv_checklist else False
         if n_clicks > 0:
-            interval_ms = interval_sec * 1000
+            interval_in_milliseconds = interval_in_seconds * 1000
             core.recorder_file = recorder_file
             core.parser.read_histories_from_recorder(core.recorder_file)
         else:
-            interval_ms = 1e9
+            interval_in_milliseconds = 1e9
         fig = core.generate_opt_history_fig()
-        return interval_ms, fig
+        return interval_in_milliseconds, fig
 
     @app.callback(
         Output('opt_hist_graph', 'extendData'),
@@ -169,7 +185,7 @@ def add_callbacks(app, core: GuiOptHistoryCore):
         Output('opt_export_html_status', 'children'),
         [Input('opt_export_html_button', 'n_clicks')],
         [State('opt_export_html_input', 'value')])
-    def export_obj_con_history_html(n_clicks, filename):
+    def export_opt_history_html(n_clicks, filename):
         status = ''
         if n_clicks > 0:
             status = core.export_fig_as_html(core.opt_hist_fig, filename)
